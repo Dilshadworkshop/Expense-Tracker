2b13478c0b7f4108642040673e8068affb90484e
DID <DID@BOTGI>
2024-09-09 20:56:21 +0530

    Add project files.

---
+++ Expense Tracker/Program.cs
@@ -0,0 +1,40 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //DI
+            builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF1cWWhAYVJyWmFZfVpgcF9DZVZTRWYuP1ZhSXxXdkxjXn9adXFXR2dUWEU=");
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
